1beb80002ac86e4534ffc01f39895be7ce45fb7e
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-02-13 00:11:51 +0600

    sequential composition

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ClassLibraryResumeProvider;
+using ClassLibraryInitialScreening;
+using ClassLibraryFinalAssessment;
+
+namespace RecruitmentSequentialComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IResumeProvider resumeProvider = new ResumeProvider();
+            IInitialScreening initialScreening = new InitialScreening();
+            IFinalAssessment finalAssessment = new FinalAssessment();
+
+            Console.WriteLine("Demonstrating Sequential Component Composition\n");
+
+            String allResumeList = resumeProvider.provideResume();
+            Console.WriteLine("<"+allResumeList+"> returned\n");
+            String selectedResumeList = initialScreening.provideSelected(allResumeList);
+            Console.WriteLine("<"+selectedResumeList + "> returned\n");
+            String finalAcceptanceList = finalAssessment.acceptanceList(selectedResumeList);
+            Console.WriteLine("<"+finalAcceptanceList + "> returned\n");
+
+        }
+    }
+}
