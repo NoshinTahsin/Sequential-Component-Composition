6a57eb70c601624221e0f9665fcc0d40e44768b4
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-02-12 22:37:50 +0600

    resume collector component

---
+++ ClassLibraryResumeCollector/ClassLibraryResumeCollector/ClassLibraryResumeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//created a c# class library
+//interface for resume provider class
+namespace ClassLibraryResumeProvider
+{
+    public class ResumeProvider: IResumeProvider
+    {
+        public String provideResume() {
+
+            return "Returned list of collected resumes.";
+            
+        }
+
+    }
+}
