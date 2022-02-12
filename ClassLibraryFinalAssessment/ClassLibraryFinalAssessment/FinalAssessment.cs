159e72b55a49be03def2b938e843bce1d266ec0c
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-02-12 23:35:37 +0600

    component for final assessment

---
+++ ClassLibraryFinalAssessment/ClassLibraryFinalAssessment/FinalAssessment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryFinalAssessment
+{
+    public class FinalAssessment:IFinalAssessment
+    {
+        public String acceptanceList(String selectedResumeList)
+        {
+            return "Acceptance list after final assessment of the " + selectedResumeList;
+        }
+    }
+}
