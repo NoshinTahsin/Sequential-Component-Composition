7ce3abb1835218c97ce24ea41fff542d3eb14905
NoshinTahsin <35559870+NoshinTahsin@users.noreply.github.com>
2022-02-12 23:01:04 +0600

    initial resume screening component

---
+++ ClassLibraryInitialScreening/ClassLibraryInitialScreening/InitialScreening.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibraryInitialScreening
+{
+    public class InitialScreening
+    {
+        public String provideSelected()
+        {
+            return "Provide selected resume list after initial screening.";
+        }
+    }
+}
